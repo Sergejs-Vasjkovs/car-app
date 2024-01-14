e45ada4b9d768f10b991e2645fc2da3583aee3a8
Sergejs-Vasjkovs <s.vasjkovs@gmail.com>
2024-01-14 19:16:33 +0200

    complete Car-app with Next JS

---
+++ app/layout.tsx
@@ -0,0 +1,20 @@
+import type { Metadata } from "next";
+import "./globals.css";
+import { Footer, NavBar } from "@/components";
+
+export const metadata: Metadata = {
+    title: "Car Hub",
+    description: "Cool web page on Next JS",
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="en">
+            <body className="relative">
+                <NavBar />
+                {children}
+                <Footer />
+            </body>
+        </html>
+    );
+}
---
+++ types/index.ts
@@ -0,0 +1,58 @@
+import { MouseEventHandler } from "react";
+
+export interface CustomButtonProps {
+    title: String;
+    containerStyles?: string;
+    textStyles?: string;
+    rightIcon?: string;
+    btnType?: "button" | "submit";
+    isDisable?: boolean;
+    handleClick?: MouseEventHandler<HTMLButtonElement>;
+}
+
+export interface SearchManufacturerProps {
+    manufacturer: String;
+    setManufacturer: (manufacturer: string) => void;
+}
+
+export interface CarProps {
+    city_mpg: number;
+    class: string;
+    combination_mpg: number;
+    cylinders: number;
+    displacement: number;
+    drive: string;
+    fuel_type: string;
+    highway_mpg: number;
+    make: string;
+    model: string;
+    transmission: string;
+    year: number;
+}
+
+export interface FilterProps {
+    manufacturer: string;
+    year: number;
+    fuel: string;
+    limit: number;
+    model: string;
+}
+
+export interface OptionsProps {
+    title: string;
+    value: string;
+}
+
+export interface CustomFilterProps {
+    title: string;
+    options: OptionsProps[];
+}
+
+export interface ShowMoreProps {
+    pageNumber: number;
+    isNext: boolean;
+}
+
+export interface HomeProps {
+    searchParams: FilterProps;
+}
